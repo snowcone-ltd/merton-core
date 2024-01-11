b0253a68fbe463fa80920340c0a932d713b6e506
chrisd1100 <chrisd1100@users.noreply.github.com>
2024-01-11 13:34:27 -0500

    Add GPGX

---
+++ Genesis-Plus-GX/core.c
@@ -0,0 +1,557 @@
+#include "../core.h"
+
+#include <stdarg.h>
+
+#include "matoya.h"
+
+#include "shared.h"
+#include "md_ntsc.h"
+#include "sms_ntsc.h"
+
+#include "osd.h"
+
+#define CORE_BITMAP_W 720
+#define CORE_BITMAP_H 576
+#define CORE_SAMPLE_RATE 48000
+
+struct Core {
+	int16 audio_buffer[2048];
+	uint16_t bitmap_data[CORE_BITMAP_H][CORE_BITMAP_W];
+
+	CoreVideoFunc video_func;
+	CoreAudioFunc audio_func;
+	void *video_opaque;
+	void *audio_opaque;
+	bool loaded;
+};
+
+static CoreLogFunc CORE_LOG_FUNC;
+static void *CORE_LOG_OPAQUE;
+
+
+// From "osd.h"
+
+static void core_log(const char *fmt, ...);
+
+struct config config;
+
+char GG_ROM[1024];
+char AR_ROM[1024];
+char SK_ROM[1024];
+char SK_UPMEM[1024];
+char GG_BIOS[1024];
+char CD_BIOS_EU[1024];
+char CD_BIOS_US[1024];
+char CD_BIOS_JP[1024];
+char MS_BIOS_US[1024];
+char MS_BIOS_EU[1024];
+char MS_BIOS_JP[1024];
+
+static md_ntsc_t _md_ntsc = {0};
+md_ntsc_t *md_ntsc = &_md_ntsc;
+
+static sms_ntsc_t _sms_ntsc = {0};
+sms_ntsc_t *sms_ntsc = &_sms_ntsc;
+
+void osd_input_update(void)
+{
+	// TODO Make it atomic
+}
+
+int load_archive(char *filename, unsigned char *buffer, int maxsize, char *extension)
+{
+	size_t size = 0;
+	void *file = MTY_ReadFile(filename, &size);
+
+	if (file && size <= maxsize) {
+		memcpy(buffer, file, size);
+		MTY_Free(file);
+
+	} else {
+		size = 0;
+	}
+
+	return (int) size;
+}
+
+long crc32(unsigned long crc, const unsigned char *buf, unsigned int len)
+{
+	return MTY_CRC32(crc, buf, len);
+}
+
+
+// Core interface
+
+static const uint8_t CORE_BRM_FORMAT[0x40] = {
+	0x5F, 0x5F, 0x5F, 0x5F, 0x5F, 0x5F, 0x5F, 0x5F, 0x5F, 0x5F, 0x5F, 0x00, 0x00, 0x00, 0x00, 0x40,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x53, 0x45, 0x47, 0x41, 0x5F, 0x43, 0x44, 0x5F, 0x52, 0x4F, 0x4D, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x52, 0x41, 0x4D, 0x5F, 0x43, 0x41, 0x52, 0x54, 0x52, 0x49, 0x44, 0x47, 0x45, 0x5F, 0x5F, 0x5F,
+};
+
+static void core_log(const char *fmt, ...)
+{
+	va_list arg;
+	va_start(arg, fmt);
+
+	if (CORE_LOG_FUNC) {
+		char msg[1024];
+		vsnprintf(msg, 1024, fmt, arg);
+
+		CORE_LOG_FUNC(msg, CORE_LOG_OPAQUE);
+	}
+
+	va_end(arg);
+}
+
+static void core_reset_settings(void)
+{
+	memset(&config, 0, sizeof(config));
+
+	// Sound
+	config.psg_preamp = 150;
+	config.fm_preamp = 100;
+	config.cdda_volume = 100;
+	config.pcm_volume = 100;
+	config.hq_fm = 1;
+	config.hq_psg = 1;
+	config.filter = 2;
+	config.low_freq = 200;
+	config.high_freq = 8000;
+	config.lg = 100;
+	config.mg = 100;
+	config.hg = 100;
+	config.lp_range = 0x9999;
+	config.ym2612 = YM2612_ENHANCED;
+	// config.ym2612 = YM2612_INTEGRATED;
+	// config.ym2612 = YM2612_DISCRETE;
+	config.ym3438 = ym3438_mode_readmode;
+	// config.ym3438 = ym3438_mode_ym2612;
+
+	YM2612Config(config.ym2612);
+	OPN2_SetChipType(config.ym3438);
+
+	// System
+	config.addr_error = 1;
+	config.cd_latency = 1;
+
+	// Display
+	config.enhanced_vscroll_limit = 8;
+
+	// Input
+	for (uint8_t x = 0; x < MAX_INPUTS; x++)
+		config.input[x].padtype = DEVICE_PAD2B | DEVICE_PAD3B | DEVICE_PAD6B;
+}
+
+Core *CoreLoad(const char *name, const char *systemDir, const char *saveDir)
+{
+	Core *ctx = calloc(1, sizeof(Core));
+
+	#define FILL_BIOS(var, name) \
+		snprintf(var, sizeof(var), "%s", MTY_JoinPath(systemDir, name))
+
+	FILL_BIOS(GG_ROM, "ggenie.bin");
+	FILL_BIOS(GG_BIOS, "bios.gg");
+	FILL_BIOS(AR_ROM, "areplay.bin");
+	FILL_BIOS(SK_ROM, "sk.bin");
+	FILL_BIOS(SK_UPMEM, "sk2chip.bin");
+	FILL_BIOS(CD_BIOS_US, "bios_CD_U.bin");
+	FILL_BIOS(CD_BIOS_EU, "bios_CD_E.bin");
+	FILL_BIOS(CD_BIOS_JP, "bios_CD_J.bin");
+	FILL_BIOS(MS_BIOS_US, "bios_U.sms");
+	FILL_BIOS(MS_BIOS_EU, "bios_E.sms");
+	FILL_BIOS(MS_BIOS_JP, "bios_J.sms");
+
+	bitmap.width = CORE_BITMAP_W;
+	bitmap.height = CORE_BITMAP_H;
+	bitmap.pitch = CORE_BITMAP_W * sizeof(uint16_t);
+	bitmap.data = (unsigned char *) ctx->bitmap_data;
+
+	core_reset_settings();
+
+	input.system[0] = SYSTEM_GAMEPAD;
+	input.system[1] = SYSTEM_GAMEPAD;
+
+	return ctx;
+}
+
+void CoreUnload(Core **core)
+{
+	if (!core || !*core)
+		return;
+
+	Core *ctx = *core;
+
+	free(ctx);
+	*core = NULL;
+}
+
+void CoreSetLogFunc(Core *ctx, CoreLogFunc func, void *opaque)
+{
+	CORE_LOG_FUNC = func;
+	CORE_LOG_OPAQUE = opaque;
+}
+
+void CoreSetAudioFunc(Core *ctx, CoreAudioFunc func, void *opaque)
+{
+	ctx->audio_func = func;
+	ctx->audio_opaque = opaque;
+}
+
+void CoreSetVideoFunc(Core *ctx, CoreVideoFunc func, void *opaque)
+{
+	ctx->video_func = func;
+	ctx->video_opaque = opaque;
+}
+
+static void core_mcd_format_bram(uint8_t *bram, uint32_t size)
+{
+	if (memcmp(bram + size - 0x20, CORE_BRM_FORMAT + 0x20, 0x20)) {
+		uint8_t *ptr = bram + size - 0x40;
+		memcpy(ptr, CORE_BRM_FORMAT, 0x40);
+		memset(bram, 0x00, size - 0x40);
+
+		uint16_t page_size = MTY_Swap16((uint16_t) (size / 64 - 3));
+
+		for (uint8_t x = 0x10; x < 0x18; x += 2)
+			memcpy(ptr + x, &page_size, 2);
+	}
+}
+
+static void core_load_save_data(const void *data, size_t size)
+{
+	size_t csize = 0x10000;
+	void *ptr = sram.sram;
+
+	if (system_hw == SYSTEM_MCD) {
+		ptr = scd.bram;
+		csize = 0x2000;
+
+		core_mcd_format_bram(scd.bram, 0x2000);
+
+		// XXX External cart is not used, how to disable it?
+		if (scd.cartridge.id)
+			core_mcd_format_bram(scd.cartridge.area, 0x80000);
+	}
+
+	if (data && size <= csize)
+		memcpy(ptr, data, size);
+}
+
+bool CoreLoadGame(Core *ctx, CoreSystem system, const char *path, const void *saveData,
+	size_t saveDataSize)
+{
+	if (!load_rom((char *) path))
+		return false;
+
+	audio_init(CORE_SAMPLE_RATE, 0);
+
+	system_init();
+	core_load_save_data(saveData, saveDataSize);
+
+	system_reset();
+
+	ctx->loaded = true;
+
+	return true;
+}
+
+void CoreUnloadGame(Core *ctx)
+{
+	if (!ctx || !ctx->loaded)
+		return;
+
+	audio_shutdown();
+}
+
+bool CoreGameIsLoaded(Core *ctx)
+{
+	return ctx ? ctx->loaded : false;
+}
+
+double CoreGetFrameRate(Core *ctx)
+{
+	if (!ctx || !ctx->loaded)
+		return 60.0;
+
+	return (double) system_clock / (double) lines_per_frame / (double) MCYCLES_PER_LINE;
+}
+
+float CoreGetAspectRatio(Core *ctx)
+{
+	if (!ctx || !ctx->loaded)
+		return 1.0f;
+
+	bool is_h40 = bitmap.viewport.w == 320;
+	double dotrate = system_clock / (is_h40 ? 8.0 : 10.0);
+	double videosamplerate = 135000000.0 / 11.0;
+
+	return (videosamplerate / dotrate) * ((double) bitmap.viewport.w / ((double) bitmap.viewport.h * 2.0));
+}
+
+void CoreRun(Core *ctx)
+{
+	if (!ctx || !ctx->loaded)
+		return;
+
+	if (system_hw == SYSTEM_MCD) {
+		system_frame_scd(0);
+
+	} else {
+		system_frame_gen(0);
+	}
+
+	if (ctx->video_func) {
+		size_t o = bitmap.viewport.x + bitmap.viewport.y * bitmap.pitch;
+		uint32_t w = bitmap.viewport.w;
+
+		// XXX Oddly MD need to use the SMS NTSC filter from time to time
+		if (config.ntsc)
+			w = reg[12] & 1 ? MD_NTSC_OUT_WIDTH(w) : SMS_NTSC_OUT_WIDTH(w);
+
+		ctx->video_func(ctx->bitmap_data + o, CORE_COLOR_FORMAT_B5G6R5, w, bitmap.viewport.h,
+			bitmap.pitch, ctx->video_opaque);
+	}
+
+	if (ctx->audio_func) {
+		int frames = audio_update(ctx->audio_buffer);
+		ctx->audio_func(ctx->audio_buffer, frames, CORE_SAMPLE_RATE, ctx->audio_opaque);
+	}
+}
+
+void *CoreGetSaveData(Core *ctx, size_t *size)
+{
+	if (!ctx || !ctx->loaded)
+		return NULL;
+
+	void *ptr = NULL;
+
+	if (system_hw == SYSTEM_MCD) {
+		*size = 0x2000;
+		ptr = scd.bram;
+
+	} else if (sram.on) {
+		*size = 0x10000;
+		ptr = sram.sram;
+
+	} else {
+		return NULL;
+	}
+
+	void *sd = malloc(*size);
+	memcpy(sd, ptr, *size);
+
+	return sd;
+}
+
+void CoreReset(Core *ctx)
+{
+	if (!ctx || !ctx->loaded)
+		return;
+
+	gen_reset(0);
+}
+
+void CoreSetButton(Core *ctx, uint8_t player, CoreButton button, bool pressed)
+{
+	if (player >= MAX_INPUTS)
+		return;
+
+	uint16 b = 0;
+
+	switch (button) {
+		case CORE_BUTTON_A: b = INPUT_A; break;
+		case CORE_BUTTON_B: b = INPUT_B; break;
+		case CORE_BUTTON_X: b = INPUT_C; break;
+		case CORE_BUTTON_Y: b = INPUT_C; break;
+		case CORE_BUTTON_START: b = INPUT_START; break;
+		case CORE_BUTTON_DPAD_U: b = INPUT_UP; break;
+		case CORE_BUTTON_DPAD_D: b = INPUT_DOWN; break;
+		case CORE_BUTTON_DPAD_L: b = INPUT_LEFT; break;
+		case CORE_BUTTON_DPAD_R: b = INPUT_RIGHT; break;
+		// case CORE_BUTTON_: b = INPUT_X; break;
+		// case CORE_BUTTON_: b = INPUT_Y; break;
+		// case CORE_BUTTON_: b = INPUT_Z; break;
+		// case CORE_BUTTON_: b = INPUT_MODE; break;
+		default:
+			break;
+	}
+
+	if (pressed) {
+		input.pad[player] |= b;
+
+	} else {
+		input.pad[player] &= ~b;
+	}
+}
+
+void CoreSetAxis(Core *ctx, uint8_t player, CoreAxis axis, int16_t value)
+{
+}
+
+void *CoreGetState(Core *ctx, size_t *size)
+{
+	if (!ctx || !ctx->loaded)
+		return NULL;
+
+	*size = STATE_SIZE;
+	void *state = malloc(*size);
+
+	state_save(state);
+
+	return state;
+}
+
+bool CoreSetState(Core *ctx, const void *state, size_t size)
+{
+	if (!ctx || !ctx->loaded || size != STATE_SIZE)
+		return false;
+
+	return state_load((unsigned char *) state);
+}
+
+uint8_t CoreGetNumDisks(Core *ctx)
+{
+	// TODO
+
+	return 0;
+}
+
+int8_t CoreGetDisk(Core *ctx)
+{
+	// TODO
+
+	return -1;
+}
+
+bool CoreSetDisk(Core *ctx, int8_t disk, const char *path)
+{
+
+	// TODO
+
+	return false;
+}
+
+
+// Settings
+
+#define CORE_SETTING_PREFIX "genesis-plus-gx-"
+
+#define CMP_SETTING(key, suffix) \
+	!strcmp(key, CORE_SETTING_PREFIX suffix)
+
+#define CMP_BOOL(b) \
+	!strcmp(b, "true")
+
+#define CMP_ENUM(e, val) \
+	!strcmp(e, val)
+
+static const CoreSetting CORE_SETTINGS[] = {{
+	.type = CORE_SETTING_ENUM,
+	.group = CORE_SETTING_GROUP_AUDIO,
+	.desc = "Low-Pass Filter",
+	.key = CORE_SETTING_PREFIX "lpf",
+	.opts = {"Off", "Simple", "3-Pass"},
+	.nopts = 3,
+}, {
+	.type = CORE_SETTING_ENUM,
+	.group = CORE_SETTING_GROUP_VIDEO,
+	.desc = "NTSC Filter",
+	.key = CORE_SETTING_PREFIX "ntsc",
+	.opts = {"Off", "Monochrome", "Composite", "S-Video", "RGB"},
+	.nopts = 5,
+}, {
+	.type = CORE_SETTING_BOOL,
+	.group = CORE_SETTING_GROUP_AUDIO,
+	.desc = "Stereo",
+	.key = CORE_SETTING_PREFIX "stereo",
+}, {
+	.type = CORE_SETTING_BOOL,
+	.group = CORE_SETTING_GROUP_VIDEO,
+	.desc = "Disable Sprite Limit",
+	.key = CORE_SETTING_PREFIX "no-sprite-limit",
+}};
+
+const CoreSetting *CoreGetAllSettings(Core *ctx, uint32_t *len)
+{
+	*len = sizeof(CORE_SETTINGS) / sizeof(CoreSetting);
+
+	return CORE_SETTINGS;
+}
+
+const char *CoreGetSetting(Core *ctx, const char *key)
+{
+	if (!ctx)
+		return NULL;
+
+	if (CMP_SETTING(key, "lpf")) {
+		return config.filter == 2 ? "3-Pass" :
+			config.filter == 1 ? "Simple" : "Off";
+
+	} else if (CMP_SETTING(key, "ntsc")) {
+		return config.ntsc == 1 ? "Monochrome" :
+			config.ntsc == 2 ? "Composite" :
+			config.ntsc == 3 ? "S-Video" :
+			config.ntsc == 4 ? "RGB" :
+			"Off";
+
+	} else if (CMP_SETTING(key, "stereo")) {
+		return config.mono == 0 ? "true" : "false";
+
+	} else if (CMP_SETTING(key, "no-sprite-limit")) {
+		return config.no_sprite_limit == 1 ? "true" : "false";
+	}
+
+	return NULL;
+}
+
+void CoreSetSetting(Core *ctx, const char *key, const char *val)
+{
+	if (!ctx)
+		return;
+
+	if (CMP_SETTING(key, "lpf")) {
+		config.filter = CMP_ENUM(val, "3-Pass") ? 2 :
+			CMP_ENUM(val, "Simple") ? 1 : 0;
+
+	} else if (CMP_SETTING(key, "ntsc")) {
+		config.ntsc = CMP_ENUM(val, "Monochrome") ? 1 :
+			CMP_ENUM(val, "Composite") ? 2 :
+			CMP_ENUM(val, "S-Video") ? 3 :
+			CMP_ENUM(val, "RGB") ? 4 :
+			0;
+
+		switch (config.ntsc) {
+			case 1:
+				md_ntsc_init(md_ntsc, &md_ntsc_monochrome);
+				sms_ntsc_init(sms_ntsc, &sms_ntsc_monochrome);
+				break;
+			case 2:
+				md_ntsc_init(md_ntsc, &md_ntsc_composite);
+				sms_ntsc_init(sms_ntsc, &sms_ntsc_composite);
+				break;
+			case 3:
+				md_ntsc_init(md_ntsc, &md_ntsc_svideo);
+				sms_ntsc_init(sms_ntsc, &sms_ntsc_svideo);
+				break;
+			case 4:
+				md_ntsc_init(md_ntsc, &md_ntsc_rgb);
+				sms_ntsc_init(sms_ntsc, &sms_ntsc_rgb);
+				break;
+		}
+
+	} else if (CMP_SETTING(key, "stereo")) {
+		config.mono = CMP_BOOL(val) ? 0 : 1;
+
+	} else if (CMP_SETTING(key, "no-sprite-limit")) {
+		config.no_sprite_limit = CMP_BOOL(val) ? 1 : 0;
+	}
+}
+
+void CoreResetSettings(Core *ctx)
+{
+	if (!ctx)
+		return;
+
+	core_reset_settings();
+}
---
+++ Genesis-Plus-GX/osd.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#define MAX_INPUTS 8
+
+#define HAVE_NO_SPRITE_LIMIT
+#define MAX_SPRITES_PER_LINE 80
+#define TMS_MAX_SPRITES_PER_LINE (config.no_sprite_limit ? MAX_SPRITES_PER_LINE : 4)
+#define MODE4_MAX_SPRITES_PER_LINE (config.no_sprite_limit ? MAX_SPRITES_PER_LINE : 8)
+#define MODE5_MAX_SPRITES_PER_LINE (config.no_sprite_limit ? MAX_SPRITES_PER_LINE : (bitmap.viewport.w >> 4))
+#define MODE5_MAX_SPRITE_PIXELS (config.no_sprite_limit ? MAX_SPRITES_PER_LINE * 32 : max_sprite_pixels)
+
+void osd_input_update(void);
+int load_archive(char *filename, unsigned char *buffer, int maxsize, char *extension);
+long crc32(unsigned long crc, const unsigned char *buf, unsigned int len);
+
+struct config {
+	uint8 hq_fm;
+	uint8 filter;
+	uint8 hq_psg;
+	uint8 ym2612;
+	uint8 ym2413;
+	uint8 ym3438;
+	uint8 cd_latency;
+	int16 psg_preamp;
+	int16 fm_preamp;
+	int16 cdda_volume;
+	int16 pcm_volume;
+	uint32 lp_range;
+	int16 low_freq;
+	int16 high_freq;
+	int16 lg;
+	int16 mg;
+	int16 hg;
+	uint8 mono;
+	uint8 system;
+	uint8 region_detect;
+	uint8 vdp_mode;
+	uint8 master_clock;
+	uint8 force_dtack;
+	uint8 addr_error;
+	uint8 bios;
+	uint8 lock_on;
+	uint8 add_on;
+	uint8 overscan;
+	uint8 gg_extra;
+	uint8 ntsc;
+	uint8 lcd;
+	uint8 render;
+	uint8 enhanced_vscroll;
+	uint8 enhanced_vscroll_limit;
+	uint8 no_sprite_limit;
+	struct {
+		uint8 padtype;
+	} input[MAX_INPUTS];
+};
+
+extern struct config config;
+
+extern char GG_ROM[1024];
+extern char GG_BIOS[1024];
+extern char AR_ROM[1024];
+extern char SK_ROM[1024];
+extern char SK_UPMEM[1024];
+extern char CD_BIOS_EU[1024];
+extern char CD_BIOS_US[1024];
+extern char CD_BIOS_JP[1024];
+extern char MS_BIOS_US[1024];
+extern char MS_BIOS_EU[1024];
+extern char MS_BIOS_JP[1024];
