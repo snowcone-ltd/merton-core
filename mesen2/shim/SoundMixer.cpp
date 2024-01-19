54683d4213096c40ad22525b154df1bc33792a97
chrisd1100 <chrisd1100@users.noreply.github.com>
2024-01-18 21:18:33 -0500

    Mesen2: Only raise volume of the NES

--- mesen2/shim/SoundMixer.cpp
+++ mesen2/shim/SoundMixer.cpp
@@ -3,6 +3,7 @@
 #include "Shared/Audio/SoundResampler.h"
 #include "Shared/Audio/WaveRecorder.h"
 #include "Shared/Interfaces/IAudioProvider.h"
+#include "Shared/Emulator.h"
 #include "Utilities/Audio/Equalizer.h"
 #include "Utilities/Audio/ReverbFilter.h"
 #include "Utilities/Audio/CrossFeedFilter.h"
@@ -15,6 +16,7 @@ static uint32_t SOUND_MIXER_SAMPLE_RATE;
 
 SoundMixer::SoundMixer(Emulator* emu)
 {
+	_emu = emu;
 }
 
 SoundMixer::~SoundMixer()
@@ -46,9 +48,13 @@ void SoundMixer::PlayAudioBuffer(int16_t* samples, uint32_t sampleCount, uint32_
 	SOUND_MIXER_SAMPLE_RATE = sourceRate;
 
 	if (SOUND_MIXER_AUDIO_FUNC) {
-		// Audio volume seems to be lower compared to previous versions of Mesen / other emulators?
-		for (uint32_t x = 0; x < sampleCount * 2; x++)
-			samples[x] = lrint(samples[x] * 1.6);
+		ConsoleType t = _emu->GetConsole()->GetConsoleType();
+
+		if (t == ConsoleType::Nes) {
+			// Audio volume seems to be lower compared to previous versions of Mesen / other emulators?
+			for (uint32_t x = 0; x < sampleCount * 2; x++)
+				samples[x] = lrint(samples[x] * 1.6);
+		}
 
 		for (IAudioProvider *provider : _audioProviders)
 			provider->MixAudio(samples, sampleCount, sourceRate);
