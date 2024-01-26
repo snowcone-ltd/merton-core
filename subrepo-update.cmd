7bddcee2c435fd658dce0503fab0b508d7d31d02
chrisd1100 <chrisd1100@users.noreply.github.com>
2024-01-26 14:12:24 -0500

    macOS makefile tweaks

--- subrepo-update.cmd
+++ subrepo-update.cmd
@@ -1,31 +1,31 @@
-@git clone git@github.com:stenzek/duckstation duckstation/duckstation
-@git -C duckstation/duckstation reset --hard HEAD
-@git -C duckstation/duckstation pull
+git clone git@github.com:stenzek/duckstation duckstation/duckstation
+git -C duckstation/duckstation reset --hard HEAD
+git -C duckstation/duckstation pull
 
-@git clone git@github.com:ekeeke/Genesis-Plus-GX genesis-plus-gx/Genesis-Plus-GX
-@git -C genesis-plus-gx/Genesis-Plus-GX reset --hard HEAD
-@git -C genesis-plus-gx/Genesis-Plus-GX pull
+git clone git@github.com:ekeeke/Genesis-Plus-GX genesis-plus-gx/Genesis-Plus-GX
+git -C genesis-plus-gx/Genesis-Plus-GX reset --hard HEAD
+git -C genesis-plus-gx/Genesis-Plus-GX pull
 
-@git clone git@github.com:SourMesen/Mesen2 mesen2/Mesen2
-@git -C mesen2/Mesen2 reset --hard HEAD
-@git -C mesen2/Mesen2 pull
+git clone git@github.com:SourMesen/Mesen2 mesen2/Mesen2
+git -C mesen2/Mesen2 reset --hard HEAD
+git -C mesen2/Mesen2 pull
 
-@git clone git@github.com:mgba-emu/mgba mgba/mgba
-@git -C mgba/mgba reset --hard HEAD
-@git -C mgba/mgba pull
+git clone git@github.com:mgba-emu/mgba mgba/mgba
+git -C mgba/mgba reset --hard HEAD
+git -C mgba/mgba pull
 
-@git clone git@github.com:mupen64plus/mupen64plus-core mupen64plus/mupen64plus-core
-@git -C mupen64plus/mupen64plus-core reset --hard HEAD
-@git -C mupen64plus/mupen64plus-core pull
+git clone git@github.com:mupen64plus/mupen64plus-core mupen64plus/mupen64plus-core
+git -C mupen64plus/mupen64plus-core reset --hard HEAD
+git -C mupen64plus/mupen64plus-core pull
 
-@git clone git@github.com:ata4/angrylion-rdp-plus mupen64plus/angrylion-rdp-plus
-@git -C mupen64plus/angrylion-rdp-plus reset --hard HEAD
-@git -C mupen64plus/angrylion-rdp-plus pull
+git clone git@github.com:ata4/angrylion-rdp-plus mupen64plus/angrylion-rdp-plus
+git -C mupen64plus/angrylion-rdp-plus reset --hard HEAD
+git -C mupen64plus/angrylion-rdp-plus pull
 
-@git clone git@github.com:mupen64plus/mupen64plus-rsp-cxd4 mupen64plus/mupen64plus-rsp-cxd4
-@git -C mupen64plus/mupen64plus-rsp-cxd4 reset --hard HEAD
-@git -C mupen64plus/mupen64plus-rsp-cxd4 pull
+git clone git@github.com:mupen64plus/mupen64plus-rsp-cxd4 mupen64plus/mupen64plus-rsp-cxd4
+git -C mupen64plus/mupen64plus-rsp-cxd4 reset --hard HEAD
+git -C mupen64plus/mupen64plus-rsp-cxd4 pull
 
-@git clone git@github.com:stella-emu/stella stella/stella
-@git -C stella/stella reset --hard HEAD
-@git -C stella/stella pull
+git clone git@github.com:stella-emu/stella stella/stella
+git -C stella/stella reset --hard HEAD
+git -C stella/stella pull
