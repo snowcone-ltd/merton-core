2023312b716293d413afffc0dd630e5af65913eb
chrisd1100 <chrisd1100@users.noreply.github.com>
2024-05-31 08:07:31 -0400

    Bring some repos current

--- subrepo-update.cmd
+++ subrepo-update.cmd
@@ -6,7 +6,7 @@ git -C duckstation/duckstation checkout faa64ec4ffd08ebb6ee9e378a23717a5c3310d1b
 git clone git@github.com:ekeeke/Genesis-Plus-GX genesis-plus-gx/Genesis-Plus-GX
 git -C genesis-plus-gx/Genesis-Plus-GX fetch
 git -C genesis-plus-gx/Genesis-Plus-GX reset --hard HEAD
-git -C genesis-plus-gx/Genesis-Plus-GX checkout 8c72ba0d4557aab4bbbae05a0c6b1382d23b4e74
+git -C genesis-plus-gx/Genesis-Plus-GX checkout f9f16d7a552c5606caea52c2179ca2831b8e0177
 
 git clone git@github.com:SourMesen/Mesen2 mesen2/Mesen2
 git -C mesen2/Mesen2 fetch
@@ -21,17 +21,17 @@ git -C mgba/mgba checkout 3571b112dc821d3612e5073afb8b6be41a35dc55
 git clone git@github.com:mupen64plus/mupen64plus-core mupen64plus/mupen64plus-core
 git -C mupen64plus/mupen64plus-core fetch
 git -C mupen64plus/mupen64plus-core reset --hard HEAD
-git -C mupen64plus/mupen64plus-core checkout 860fac3fbae94194a392c1d9857e185eda6d083e
+git -C mupen64plus/mupen64plus-core checkout 5340dafcc0f5e8284057ab931dd5c66222d3d49e
 
 git clone git@github.com:ata4/angrylion-rdp-plus mupen64plus/angrylion-rdp-plus
 git -C mupen64plus/angrylion-rdp-plus fetch
 git -C mupen64plus/angrylion-rdp-plus reset --hard HEAD
-git -C mupen64plus/angrylion-rdp-plus checkout 20eaeaffc83ee7a4ca64a533a65f906b82d10dc4
+git -C mupen64plus/angrylion-rdp-plus checkout 8a1cc860b1afcba8ac80f9569ca46ba0f8fbddd3
 
 git clone git@github.com:mupen64plus/mupen64plus-rsp-cxd4 mupen64plus/mupen64plus-rsp-cxd4
 git -C mupen64plus/mupen64plus-rsp-cxd4 fetch
 git -C mupen64plus/mupen64plus-rsp-cxd4 reset --hard HEAD
-git -C mupen64plus/mupen64plus-rsp-cxd4 0a4e30f56033396e3ba47ec0fdd7acea3522362a
+git -C mupen64plus/mupen64plus-rsp-cxd4 checkout f6ff3719cb68d3e1c1497fc87a661921671db719
 
 git clone git@github.com:Themaister/parallel-rdp-standalone mupen64plus/parallel-rdp
 git -C mupen64plus/parallel-rdp fetch
@@ -41,7 +41,7 @@ git -C mupen64plus/parallel-rdp checkout 37bf2967e3dd18e059cf5563717bb6c88c261f3
 git clone git@github.com:libretro/parallel-rsp mupen64plus/parallel-rsp
 git -C mupen64plus/parallel-rsp fetch
 git -C mupen64plus/parallel-rsp reset --hard HEAD
-git -C mupen64plus/parallel-rsp checkout 5b0f81bd08ad6bca177e2713237697b04282dbe4
+git -C mupen64plus/parallel-rsp checkout 8f1d59d5393326358499edb8ecc8a328c680ce43
 
 git clone git@github.com:stella-emu/stella stella/stella
 git -C stella/stella fetch
