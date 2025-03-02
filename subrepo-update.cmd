103cc81ec58018cbfe818fdde654f678749d9687
chrisd1100 <chrisd1100@users.noreply.github.com>
2025-03-01 20:36:13 -0500

    Bring GPGX current

--- subrepo-update.cmd
+++ subrepo-update.cmd
@@ -6,7 +6,7 @@ git -C duckstation/duckstation checkout 5a185654ba5d50ad137a5f7fc4c80854e66addb0
 git clone git@github.com:ekeeke/Genesis-Plus-GX genesis-plus-gx/Genesis-Plus-GX
 git -C genesis-plus-gx/Genesis-Plus-GX fetch
 git -C genesis-plus-gx/Genesis-Plus-GX reset --hard HEAD
-git -C genesis-plus-gx/Genesis-Plus-GX checkout f9f16d7a552c5606caea52c2179ca2831b8e0177
+git -C genesis-plus-gx/Genesis-Plus-GX checkout a203bbf2ab6c7164051ff90ad30cc8f6352a56ac
 
 git clone git@github.com:SourMesen/Mesen2 mesen2/Mesen2
 git -C mesen2/Mesen2 fetch
