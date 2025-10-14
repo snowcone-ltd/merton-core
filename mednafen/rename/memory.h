323dcb53877c642f6a4b8f2e365c36b768681858
chrisd1100 <chrisd1100@users.noreply.github.com>
2025-10-14 13:24:44 -0400

    Windows cleanup

--- mednafen/rename/memory.h
+++ mednafen/rename/memory.h
@@ -1,2 +1,5 @@
-//#include "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.26100.0\\ucrt\\memory.h"
-#include "/Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/memory.h"
+#if defined(_WIN32)
+	#include "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.26100.0\\ucrt\\memory.h"
+#else
+	#include "/Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/memory.h"
+#endif
--- mednafen/rename/time.h
+++ mednafen/rename/time.h
@@ -1,2 +1,5 @@
-//#include "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.26100.0\\ucrt\\time.h"
-#include "/Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/time.h"
+#if defined(_WIN32)
+	#include "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.26100.0\\ucrt\\time.h"
+#else
+	#include "/Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/time.h"
+#endif
